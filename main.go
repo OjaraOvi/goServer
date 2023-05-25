49adff7e1428641c2ad714f5fe5d1c7a82ebe048
Octavio Jara <ocjara.oviedo@gmail.com>
2023-05-25 00:17:00 -0400

    feat: add log middleware

--- main.go
+++ main.go
@@ -3,6 +3,6 @@ package main
 func main () {
 	server := NewServer(":3000")
 	server.Handle("/", HandleRoot)
-	server.Handle("/api", server.AddMiddleware(HanldeHome, CheckAuth()))
+	server.Handle("/api", server.AddMiddleware(HanldeHome, CheckAuth(), Logging()))
 	server.Listen()
 }
--- middleware.go
+++ middleware.go
@@ -2,19 +2,33 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
+	"time"
 )
 
 func CheckAuth() Middleware {
 	return func (f http.HandlerFunc) http.HandlerFunc{
 		return func (w http.ResponseWriter, r *http.Request){
-			flag := false
+			flag := true
 			fmt.Println("checking authentication")
 			if flag {
 				f(w,r)
 			}else {
 				return
 			}
+		}
+	}
+}
+
+func Logging() Middleware {
+	return func (f http.HandlerFunc) http.HandlerFunc{
+		return func (w http.ResponseWriter, r *http.Request){
+			start := time.Now()
+			defer func () {
+				log.Println(r.URL.Path, time.Since(start))
+			}()
+			f(w,r)
 		}
 	}
 }
