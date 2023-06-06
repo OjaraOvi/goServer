8889cd2109e90619f6adb547a3060614ba717bda
Octavio Jara <ocjara.oviedo@gmail.com>
2023-06-05 22:45:51 -0400

    feat: add methods and json operators

--- handlers.go
+++ handlers.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
 )
@@ -11,4 +12,35 @@ func HandleRoot(w http.ResponseWriter, r * http.Request) {
 
 func HanldeHome(w http.ResponseWriter, r * http.Request) {
 	fmt.Fprintf(w, "api endpoint")
+}
+
+func PostRequest (w http.ResponseWriter, r * http.Request){
+	decoder := json.NewDecoder(r.Body)
+	var metaData MetaData
+	err := decoder.Decode(&metaData)
+	if err != nil {
+		fmt.Fprintf(w, "error: %v", err)
+		return
+	}
+	
+	fmt.Fprintf(w, "PayLoad %v", metaData)
+}
+
+
+func UserPostRequest (w http.ResponseWriter, r * http.Request){
+	decoder := json.NewDecoder(r.Body)
+	var user User
+	err := decoder.Decode(&user)
+	if err != nil {
+		fmt.Fprintf(w, "error: %v", err)
+		return
+	}
+	response, err := user.ToJson()
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	w.Header().Set("Content-Type", "Application/json")
+	w.Write(response)
+	//fmt.Fprintf(w, "PayLoad %v", user)
 }
